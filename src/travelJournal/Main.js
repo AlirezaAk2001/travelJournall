2b0c05b01e9c566511b5903140eef4c4714321bd
Alireza <163636227+AlirezaAk2001@users.noreply.github.com>
2025-07-11 21:55:05 +0330

    Update Main.js

--- src/travelJournal/Main.js
+++ src/travelJournal/Main.js
@@ -3,18 +3,18 @@ export default function Main ({img, countryName, link, placeName, date, info}) {
         <main>
             <section>
                 <img id="place" src={img} alt="name"></img>
-                 <div className="info">
+                <div className="info">
                     <img id="mark" src="./img/marker.png" alt="location"></img>
                     <span>{countryName}</span>
-                 <a href={link}>
-                    View on Google Maps
-                 </a>
-                 </div>
-                 <div className="info-m-f">
+                     <a href={link}>
+                        View on Google Maps
+                     </a>
+                </div>
+                <div className="info-m-f">
                     <h1>{placeName}</h1>
                   <big><p>{date}</p></big>
                   <span>{info}</span>
-                  </div>
+                </div>
             </section>
         </main>
     )
